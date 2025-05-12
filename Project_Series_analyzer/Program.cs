57e0c0817c0f3fb6915ffee0fd1758c06caf3982
Moti <shalom7950@gmail.com>
2025-05-12 15:48:26 +0300

    Start with a func

---
+++ Project_Series_analyzer/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project_Series_analyzer
+{
+    internal class Program
+    {
+        static void getSeries()
+        { }
+        static void reverceSeries()
+        { }
+        static void getMax()
+        { }
+        static void getMin()
+        { }
+        static void getAverage()
+        { }
+        static void getLength()
+        { }
+        static void getSumOfTheSeries()
+        { }
+        static void orgivingSeries()
+        { }
+        static void Exit()
+        { }
+        static void Main(string[] args)
+        {
+        
+        }
+    
+    }
+}
