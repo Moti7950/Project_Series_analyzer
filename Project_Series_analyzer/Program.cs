5c1522b17f3173f1b44a671921dbe01fac379b74
Moti <shalom7950@gmail.com>
2025-05-13 17:02:11 +0300

    Fixn bag on sort func

--- Project_Series_analyzer/Program.cs
+++ Project_Series_analyzer/Program.cs
@@ -8,15 +8,82 @@ namespace Project_Series_analyzer
 {
     internal class Program
     {
+        //check number ?!
+        static bool checkNumber(string[] numbers)
+        {
+            string[] numi = { "1", "2", "3", "4", "5", "6", "7", "8", "9", "0", ".", "-", "," };
+            bool check = true;
+
+            foreach (var number in numbers)
+            {
+                foreach (char c in number)
+                {
+                    if (!numi.Contains(c.ToString()))
+                    {
+                        Console.WriteLine("Not a number!!");
+                        check = false;
+                        break;
+                    }
+                    //if (c == '.' || c == '-' || c == ',')
+                    //{
+                    //    if (numi[c])
+                    //    {
+                    //        Console.WriteLine("Not a number!!");
+                    //        check = false;
+                    //        break;
+                    //    }
+                    //}
+                }
+            }
+                    if (check)
+                    {
+                        Console.WriteLine("Numbers :)");
+                    }
+             return check;
+        }
+        //a
         static void getSeries()
-        { }
+        { 
+            
+        }
+        //b
+        static void setSeries(int[] list)
+        {
+            foreach (int i in list)
+            {
+                Console.Write(i + " ");
+            }
+        }
+        //c
         static void ReverceSeries(int[] list)
         { 
             for (int i = getLength(list) -1; i >= 0 ; i--)
             {
-                Console.Write(list[i] +" ");  
+                Console.Write(list[i]+" ");  
             }
         }
+        //d
+        static void sortSeries(int[] list)
+        { 
+            for (int i = 0; i < getLength(list); i++)
+            {
+                for (int j = i + 1; j < getLength(list); j++)
+                {
+                    if (list[i] > list[j])
+                    {
+                        int tempi = list[i];
+                        list[i] = list[j];
+                        list[j] = tempi;
+                    }
+                }
+            }
+            foreach (int i in list)
+            {
+                Console.Write(i + " ");
+            }
+
+        }
+        //e
         static int getMax(int[] list)
         {
             int max = list[0];
@@ -29,6 +96,7 @@ namespace Project_Series_analyzer
             }
             return max;
         }
+        //f
         static int getMin(int[] list)
         {
             int min = list[0];
@@ -42,6 +110,7 @@ namespace Project_Series_analyzer
             return min;
         }
 
+        //g
         static int getAverage(int[] list)
         { 
             int leni = getLength(list);
@@ -50,6 +119,7 @@ namespace Project_Series_analyzer
             return average;
 
         }
+        //h
         static int getLength(int[] list)
         {
             int leni = 0;
@@ -62,6 +132,7 @@ namespace Project_Series_analyzer
             }
             return leni;
         }
+        //i
         static int getSumOfTheSeries(int[] list)
         {
             int sum = 0;
@@ -90,10 +161,11 @@ namespace Project_Series_analyzer
             //Console.WriteLine(getSumOfTheSeries(new int[] { 2, 4, 9, 3, 1, -6, 7 }));
             //Console.WriteLine(getSumOfTheSeries(new int[] {}));
             //Console.WriteLine(getAverage(new int[] { 2, 4, 9, 3, 1, -6, 7 }));
-            int[] x = { 2, 4, 9, 3, 1, -6, 7 };
-            ReverceSeries(x);
-
-
+            int[] x = { -8,2, 4, 9,-10, 3, 1, -6 };
+            //ReverceSeries(x);
+            //List<string> list = new List<string> { "1","2","2"};
+            //checkNumber(list.ToArray());
+            sortSeries(x);
         }
     
     }
